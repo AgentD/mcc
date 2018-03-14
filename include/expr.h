71063751fdb955c8789a56710f5c5c8f718473a1
David Oberhollenzer <david.oberhollenzer@student.uibk.ac.at>
2018-03-14 14:22:33 +0100

    File level cleanup
    Signed-off-by: David Oberhollenzer <david.oberhollenzer@student.uibk.ac.at>

---
+++ include/expr.h
@@ -0,0 +1,91 @@
+#ifndef MCC_EXPR_H
+#define MCC_EXPR_H
+
+#include "literal.h"
+
+typedef struct expression_t expression_t;
+typedef struct arg_t arg_t;
+
+struct arg_t {
+	expression_t *expr;
+	arg_t *next;
+};
+
+typedef enum {
+	BINOP_ADD,
+	BINOP_SUB,
+	BINOP_MUL,
+	BINOP_DIV,
+	BINOP_LESS,
+	BINOP_GREATER,
+	BINOP_LEQ,	/**< \brief Less or equal */
+	BINOP_GEQ,	/**< \brief Greater or equal */
+	BINOP_ANL,	/**< \brief Logic and */
+	BINOP_ORL,	/**< \brief Logic or */
+	BINOP_EQU,	/**< \brief Equality test */
+	BINOP_NEQU,	/**< \brief Not equal */
+
+	SEX_LITERAL,
+	SEX_IDENTIFIER,
+	SEX_ARRAY_INDEX,
+	SEX_CALL,
+	SEX_UNARY,
+} E_EXPR_TYPE;
+
+typedef enum {
+	UNARY_NEG,	/**< \brief Negation. Usually two's complement */
+	UNARY_INV,	/**< \brief Logical inverse */
+} E_UNARY;
+
+struct expression_t {
+	E_EXPR_TYPE type;
+
+	union {
+		literal_t lit;
+
+		off_t identifier;
+
+		struct {
+			off_t identifier;
+			expression_t *index;
+		} array_idx;
+
+		struct {
+			off_t identifier;
+			arg_t *args;
+		} call;
+
+		struct {
+			E_UNARY op;
+			expression_t *exp;
+		} unary;
+
+		struct {
+			expression_t *left;
+			expression_t *right;
+		} binary;
+	} u;
+};
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+expression_t *sex_literal(literal_t lit);
+expression_t *sex_identifier(off_t identifier);
+expression_t *sex_unary(E_UNARY op, expression_t *exp);
+expression_t *sex_array_access(off_t identifier, expression_t *index);
+expression_t *sex_call(off_t identifier, arg_t *args);
+
+arg_t *mkarg(expression_t *expr, arg_t *rhs);
+
+expression_t *mkexp(expression_t *left, E_EXPR_TYPE type, expression_t *right);
+
+void expr_free(expression_t *sex);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* MCC_EXPR_H */
+
---
+++ include/literal.h
@@ -0,0 +1,66 @@
+#ifndef MCC_LITERAL_H
+#define MCC_LITERAL_H
+
+#ifndef FORCE_INLINE
+	#ifdef _MSC_VER
+		#define FORCE_INLINE __forceinline
+	#else
+		#define FORCE_INLINE __inline__ __attribute__((always_inline))
+	#endif
+#endif
+
+/**
+ * \enum E_TYPE
+ *
+ * \brief Type of a literal (\ref literal_t), return type of a
+ *        function (\ref function_def_t) or variable type (\ref decl_t)
+ */
+typedef enum {
+	TYPE_VOID,	/**< \brief Only valid for \ref function_def_t */
+	TYPE_BOOL,
+	TYPE_INT,
+	TYPE_FLOAT,
+	TYPE_STRING
+} E_TYPE;
+
+typedef struct {
+	E_TYPE type;
+
+	union {
+		bool b;
+		int i;
+		float f;
+		off_t str;
+	} value;
+} literal_t;
+
+static FORCE_INLINE literal_t literal_bool(bool value)
+{
+	literal_t l = { .type = TYPE_BOOL, .value = { .b = value } };
+
+	return l;
+}
+
+static FORCE_INLINE literal_t literal_int(int value)
+{
+	literal_t l = { .type = TYPE_INT, .value = { .i = value } };
+
+	return l;
+}
+
+static FORCE_INLINE literal_t literal_float(float value)
+{
+	literal_t l = { .type = TYPE_FLOAT, .value = { .f = value } };
+
+	return l;
+}
+
+static FORCE_INLINE literal_t literal_string(off_t value)
+{
+	literal_t l = { .type = TYPE_STRING, .value = { .str = value } };
+
+	return l;
+}
+
+#endif /* MCC_LITERAL_H */
+
