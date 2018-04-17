69c70b56629349da405121d0a8be1446a54ab89a
David Oberhollenzer <david.oberhollenzer@student.uibk.ac.at>
2018-04-18 00:31:51 +0200

    Add symbol table code
    All we need is a linked list of symbols.
     - When processing a compound statement, we simply prepend to the list,
       but also store the pointer to the original head.
     - When looking for a symbol, we go through the list linearly, thus
       implementing shadowing.
     - When leaving a compound statement, we remove everything all the way
       back to the original head.
    
    Signed-off-by: David Oberhollenzer <david.oberhollenzer@student.uibk.ac.at>

---
+++ include/symtab.h
@@ -0,0 +1,64 @@
+#ifndef SYMTAB_H
+#define SYMTAB_H
+
+#include "decl.h"
+#include "literal.h"
+
+#include <stdlib.h>
+
+typedef enum {
+	SYM_TYPE_ARG = 0,
+	SYM_TYPE_VAR,
+} E_SYM_TYPE;
+
+typedef struct symbol_t {
+	E_SYM_TYPE type;
+	decl_t *decl;
+
+	struct symbol_t *next;
+} symbol_t;
+
+static FORCE_INLINE symbol_t *mcc_mksymbol(E_SYM_TYPE type, decl_t *decl)
+{
+	symbol_t *sym = calloc(1, sizeof(*sym));
+
+	if (sym != NULL) {
+		sym->type = type;
+		sym->decl = decl;
+	}
+
+	return sym;
+}
+
+static FORCE_INLINE mcc_free_symbol(symbol_t *sym)
+{
+	free(sym);
+}
+
+static FORCE_INLINE symbol_t *mcc_symtab_lookup(symbol_t *head, off_t identifier)
+{
+	while (head != NULL && head->decl->identifier != identifier)
+		head = head->next;
+
+	return head;
+}
+
+static FORCE_INLINE symbol_t *mcc_symtab_prepend(symbol_t *head, symbol_t *n)
+{
+	n->next = head;
+	return n;
+}
+
+static FORCE_INLINE symbol_t *mcc_symtab_drop(symbol_t *head)
+{
+	symbol_t *n = head;
+
+	if (n != NULL) {
+		head = n->next;
+		mcc_free_symbol(n);
+	}
+
+	return head;
+}
+
+#endif
