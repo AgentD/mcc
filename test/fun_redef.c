872efb26c20a147d3c427c8c846dcfed27875d3e
David Oberhollenzer <david.oberhollenzer@student.uibk.ac.at>
2018-04-25 14:50:39 +0200

    Add unit test for function redefinition
    Signed-off-by: David Oberhollenzer <david.oberhollenzer@student.uibk.ac.at>

---
+++ test/fun_redef.c
@@ -0,0 +1,81 @@
+#include <stdlib.h>
+
+#include "mcc.h"
+#include "test.h"
+
+const char *input =
+"int foo()\n"
+"{\n"
+"}\n"
+"\n"
+"void bar()\n"
+"{\n"
+"}\n"
+"\n"
+"void foo()\n"
+"{\n"
+"}\n";
+
+int main(void)
+{
+	semantic_result_t sem;
+	parser_result_t pr;
+	function_def_t *f;
+	str_tab_t *ident;
+
+	pr = mcc_parse_string(input);
+	TEST_ASSERT(pr.status == PARSER_STATUS_OK)
+
+	f = pr.program.functions;
+	ident = &pr.program.identifiers;
+
+	TEST_ASSERT(f != NULL)
+	TEST_ASSERT(f->type == TYPE_INT)
+	TEST_ASSERT(f->line_no == 1)
+	TEST_ASSERT(f->parameters == NULL)
+	TEST_ASSERT(f->body != NULL)
+	ASSERT_IDENTIFIER(ident, f->identifier, "foo")
+
+	f = f->next;
+
+	TEST_ASSERT(f != NULL)
+	TEST_ASSERT(f->type == TYPE_VOID)
+	TEST_ASSERT(f->line_no == 5)
+	TEST_ASSERT(f->parameters == NULL)
+	TEST_ASSERT(f->body != NULL)
+	ASSERT_IDENTIFIER(ident, f->identifier, "bar")
+
+	f = f->next;
+
+	TEST_ASSERT(f != NULL)
+	TEST_ASSERT(f->type == TYPE_VOID)
+	TEST_ASSERT(f->line_no == 9)
+	TEST_ASSERT(f->parameters == NULL)
+	TEST_ASSERT(f->body != NULL)
+	ASSERT_IDENTIFIER(ident, f->identifier, "foo")
+
+	f = f->next;
+	TEST_ASSERT(f == NULL)
+
+	sem = mcc_semantic_check(&pr.program);
+	TEST_ASSERT(sem.status == SEMANTIC_FUNCTION_REDEF)
+
+	f = sem.u.redef.first;
+	TEST_ASSERT(f != NULL)
+	TEST_ASSERT(f->type == TYPE_INT)
+	TEST_ASSERT(f->line_no == 1)
+	TEST_ASSERT(f->parameters == NULL)
+	TEST_ASSERT(f->body != NULL)
+	ASSERT_IDENTIFIER(ident, f->identifier, "foo")
+
+	f = sem.u.redef.second;
+	TEST_ASSERT(f != NULL)
+	TEST_ASSERT(f->type == TYPE_VOID)
+	TEST_ASSERT(f->line_no == 9)
+	TEST_ASSERT(f->parameters == NULL)
+	TEST_ASSERT(f->body != NULL)
+	ASSERT_IDENTIFIER(ident, f->identifier, "foo")
+
+	mcc_cleanup_program(&pr.program);
+	return EXIT_SUCCESS;
+}
